4527d3086ed3258bcf719411aba3d648c5da942f
shiv <mansotra.shiv@gmail.com>
2024-04-03 11:53:52 +0530

    added animation 6 and 7

--- src/app/animation-6/page.tsx
+++ src/app/animation-6/page.tsx
@@ -3,7 +3,7 @@
 'use client';
 
 import React, { useEffect, useRef } from 'react';
-import { gsap, Power2, Power3 } from 'gsap';
+import { gsap, Power2, Power3,Power4 } from 'gsap';
 
 function Page() {
     const items = useRef<HTMLDivElement[]>([]);
@@ -19,31 +19,39 @@ function Page() {
 
         items.current.forEach((item, index) => {
             tl.to(item, {
-                duration: 0.6,
-                css: { top: "50vh", right: `calc(50vw - 50px)`},
-                ease: Power2.easeOut,
+                duration: 1.5,
+                css: { top: "40vh", right: `calc(45vw)`},
+                ease: Power3.easeIn,
             })
             .to(item, {
-                duration: 0.4,
-                y: "-60",
-                repeat: 2,
-                yoyo: true,
-                ease: Power2.easeInOut,
+                rotation: 45,
+                duration: 0.3,
+              })
+            .to(item, {
+              duration: 0.7,
+              rotateZ: 0,
+              transformOrigin: "left top",
+              ease: Power3.easeIn,
+            })
+            .to(item, {
+              duration: 0.5,
+              rotateZ: 12,
+              repeat: 1,
+              yoyo:true,
+              transformOrigin: "left top",
+              ease: Power3.easeOut,
+            })
+            .to(item, {
+              duration: 0.4,
+              rotateZ: 5,
+              repeat: 1,
+              yoyo:true,
+              transformOrigin: "left top",
+              ease: Power3.easeOut,
             })
             .to(item, {
                 duration: 1,
-                rotation: -30,
-                ease: Power3.easeOut,
-            })
-            .to(item, {
-                duration: 1,
-                scale: 2.5,
-                rotation: 360,
-                ease: Power3.easeOut,
-            })
-            .to(item, {
-                duration: 1.2,
-                scale: 0.5, 
+                scale: 0.3, 
                 x: () => gsap.utils.random(-40, 40) + 'vw', 
                 y: () => gsap.utils.random(-30, 30) + 'vh', 
                 ease: Power3.easeInOut,
@@ -60,8 +68,8 @@ function Page() {
             {[...Array(8)].map((_, index) => (
                 <div
                     key={index}
-                    className={`rotate-[45deg] absolute bottom-[110vh] right-[50vw-50px] w-[100px] h-[100px]`}
-                    style={{ backgroundColor: `hsl(${index * 72}, 100%, 50%)` }} 
+                    className={`rotate-[45deg] rounded-lg absolute bottom-[110vh] right-[40vw] w-[150px] h-[150px]`}
+                    style={{ backgroundColor: `hsl(${index * 72}, 100%, 50%)`, transformOrigin: 'left top', }} 
                     ref={addToRefs}
                 ></div>
             ))}
